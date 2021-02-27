1f0fc960ca3f4d3305702a83fdd742f9c3abf32e
Mateus Bays <mateus@hotmail.com>
2021-02-27 00:04:48 -0300

    Adcionando context e states no app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
+import Head from 'next/head';
+import styles from '../styles/pages/Home.module.css';
 import {ExperienceBar} from "../components/ExperienceBar";
 import {Profile} from "../components/Profile";
-import styles from '../styles/pages/Home.module.css';
 import {CompletedChallenges} from "../components/CompletedChallenges";
 import {Countdown} from "../components/Countdown";
-import Head from 'next/head';
+import {ChallengeBox} from "../components/ChallengeBox";
 
 export default function Home() {
   return (
@@ -20,7 +21,7 @@ export default function Home() {
                 <Countdown/>
             </div>
             <div>
-
+                <ChallengeBox/>
             </div>
         </section>
       </div>
